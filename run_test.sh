24d477de42f1e727cf9e08250e1eb074727fcde1
linsinan1995 <mynameisxiaou@gmail.com>
2021-03-06 16:32:23 +0200

    add submodule checkout

--- run_test.sh
+++ run_test.sh
@@ -1,1 +1,20 @@
+git submodule update --init
+
+cd riscv-gcc
+git checkout riscv-gcc-10.2.0
+cd ..
+
+cd riscv-binutils
+git checkout riscv-binutils-2.35
+cd ..
+
+# Temp Fix. See:
+#    https://github.com/riscv/riscv-gnu-toolchain/issues/736
+cd qemu
+git checkout v5.2.0
+git submodule update --init --recursive
+cd ..
+
+#### MAIN SCRIIPT FOR TESTING
+#./run_test_pr84877_rv32_ilp32e.sh
 ./run_test_bug98981_rv64gc.sh
