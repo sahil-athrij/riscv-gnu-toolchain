4325ba016be3f926bfa7ed1a2304f19452f08c74
linsinan1995 <mynameisxiaou@gmail.com>
2021-03-19 09:43:24 +0200

    fix submodule update

--- env_setup_p_ext.sh
+++ env_setup_p_ext.sh
@@ -1,13 +1,18 @@
 git submodule update --init
 
+gcc_branch=p-ext-dev
+binutils_branch=p-ext-dev
+
 cd riscv-gcc
-git pull https://github.com/linsinan1995/riscv-gcc.git p-ext-dev
-git checkout p-ext-dev
+git remote | grep -q $gcc_branch || git remote add $gcc_branch https://github.com/linsinan1995/riscv-gcc.git
+git fetch $gcc_branch
+git checkout -f $gcc_branch/$gcc_branch
 cd ..
 
 cd riscv-binutils
-git pull https://github.com/linsinan1995/riscv-binutils-gdb.git p-ext-dev
-git checkout p-ext-dev
+git remote | grep -q $binutils_branch || git remote add $binutils_branch https://github.com/linsinan1995/riscv-binutils-gdb.git 
+git fetch $binutils_branch
+git checkout -f $binutils_branch/$binutils_branch
 cd ..
 
 # Temp Fix. See:
